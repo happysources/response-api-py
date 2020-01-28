9e240ec3a5386b7547028b7fec0c4533c27ac972
Erik Brozek <erik@brozek.name>
2020-01-28 09:47:47 +0100

    Fixed: pylint refactoring - Too many arguments + Method could be a function

--- response_api/response_api.py
+++ response_api/response_api.py
@@ -13,8 +13,16 @@ import http
 class ResponseAPI(object):
 	""" ResponseAPI object """
 
-	def __response(self, httpstatus, message='', time_ms=0, found=0, data=(), error_dict=None):
+	_pylint_fixed = 0
+
+	def __response(self, httpstatus, param):
 		""" structure for body response """
+
+		self._pylint_fixed = 0
+
+		message = param.get('message')
+		time_ms = param.get('time_ms')
+		found = param.get('found', 0)
 
 		if not message:
 			message = httpstatus.phrase
@@ -25,77 +33,111 @@ class ResponseAPI(object):
 				'found' : found,\
 				'time_ms' : time_ms}}
 
-		if data:
-			ret['data'] = data
+		if param.get('data'):
+			ret['data'] = param.get('data')
 
 			if found == 0:
-				ret['data'] = len(data)
+				ret['data'] = len(param.get('data'))
 
-		if error_dict:
-			ret['error'] = error_dict
+		if param.get('error_dict'):
+			ret['error'] = param.get('error_dict')
 
 		return ret
 
+
 	# 2xx
 	def ok(self, message='', time_ms=0, found=0, data=()):
-		""" response 200 OK """
-		return self.__response(http.HTTPStatus.OK, message, time_ms, found, data)
+		""" 200 OK """
+
+		return self.__response(http.HTTPStatus.OK,\
+			{'message':message, 'time_ms':time_ms, 'found':found, 'data':data})
+
 
 	def created(self, message='', time_ms=0, found=0, data=()):
-		""" response 201 CREATED """
-		return self.__response(http.HTTPStatus.CREATED, message, time_ms, found, data)
+		""" 201 CREATED """
+
+		return self.__response(http.HTTPStatus.CREATED,\
+			{'message':message, 'time_ms':time_ms, 'found':found, 'data':data})
+
 
 	def accepted(self, message='', time_ms=0, found=0, data=()):
-		""" response 202 ACCEPTED """
-		return self.__response(http.HTTPStatus.ACCEPTED, message, time_ms, found, data)
+		""" 202 ACCEPTED """
+
+		return self.__response(http.HTTPStatus.ACCEPTED,\
+			{'message':message, 'time_ms':time_ms, 'found':found, 'data':data})
+
 
 	def no_content(self, message='', time_ms=0, found=0):
-		""" response 204 NO CONTENT """
-		return self.__response(http.HTTPStatus.NO_CONTENT, message, time_ms, found)
+		""" 204 NO CONTENT """
+
+		return self.__response(http.HTTPStatus.NO_CONTENT,\
+			{'message':message, 'time_ms':time_ms, 'found':found})
+
 
 	# 3xx
 	def moved_permanently(self, message='', time_ms=0):
-		""" response 301 MOVED PERMANENTLY """
-		return self.__response(http.HTTPStatus.MOVED_PERMANENTLY, message, time_ms)
+		""" 301 MOVED PERMANENTLY """
+
+		return self.__response(http.HTTPStatus.MOVED_PERMANENTLY,\
+			{'message':message, 'time_ms':time_ms})
+
 
 	def found(self, message='', time_ms=0, error_dict=None):
-		""" response 302 FOUND """
-		return self.__response(http.HTTPStatus.FOUND, message, time_ms, error_dict=error_dict)
+		""" 302 FOUND """
 
-	def redirect(self, message='', time_ms=0):
+		return self.__response(http.HTTPStatus.FOUND,\
+			{'message':message, 'time_ms':time_ms, 'error_dict':error_dict})
+
+	def redirect(self, message='', time_ms=0, error_dict=None):
 		""" alias for found() """
-		return self.found(message, time_ms)
+		return self.found(message, time_ms, error_dict=error_dict)
+
 
 	def not_modified(self, message='', time_ms=0):
 		""" alias for not_modified() """
-		return self.__response(http.HTTPStatus.NOT_MODIFIED, message, time_ms)
+
+		return self.__response(http.HTTPStatus.NOT_MODIFIED,\
+			{'message':message, 'time_ms':time_ms})
+
 
 	# 4xx
 	def bad_request(self, message='', time_ms=0, error_dict=None):
-		""" response 400 Bad request """
-		return self.__response(http.HTTPStatus.BAD_REQUEST, message, time_ms, error_dict=error_dict)
+		""" 400 Bad request """
 
-	def unauthorized(self, message='', time_ms=0):
-		""" response 401 UNAUTHORIZED """
-		return self.__response(http.HTTPStatus.UNAUTHORIZED, message, time_ms)
+		return self.__response(http.HTTPStatus.BAD_REQUEST,\
+			{'message':message, 'time_ms':time_ms, 'error_dict':error_dict})
 
-	def unauth(self, message='', time_ms=0):
+	def unauthorized(self, message='', time_ms=0, error_dict=None):
+		""" 401 UNAUTHORIZED """
+
+		return self.__response(http.HTTPStatus.UNAUTHORIZED,\
+			{'message':message, 'time_ms':time_ms, 'error_dict':error_dict})
+
+	def unauth(self, message='', time_ms=0, error_dict=None):
 		""" alias for unauthorized() """
-		return self.unauthorized(message, time_ms)
+		return self.unauthorized(message, time_ms, error_dict=error_dict)
 
-	def bad_forbidden(self, message='', time_ms=0):
-		""" response 403 FORBIDDEN """
-		return self.__response(http.HTTPStatus.FORBIDDEN, message, time_ms)
+
+	def bad_forbidden(self, message='', time_ms=0, error_dict=None):
+		""" 403 FORBIDDEN """
+
+		return self.__response(http.HTTPStatus.FORBIDDEN,\
+			{'message':message, 'time_ms':time_ms, 'error_dict':error_dict})
+
 
 	def not_found(self, message='', time_ms=0, error_dict=None):
-		""" response 404 Not found """
-		return self.__response(http.HTTPStatus.NOT_FOUND, message, time_ms, error_dict=error_dict)
+		""" 404 Not found """
+
+		return self.__response(http.HTTPStatus.NOT_FOUND,\
+			{'message':message, 'time_ms':time_ms, 'error_dict':error_dict})
 
 
 	# 5xx
 	def internal_server_error(self, message='', time_ms=0):
-		""" response 500 INTERNAL SERVER ERROR """
-		return self.__response(http.HTTPStatus.INTERNAL_SERVER_ERROR, message, time_ms)
+		""" 500 INTERNAL SERVER ERROR """
+
+		return self.__response(http.HTTPStatus.INTERNAL_SERVER_ERROR,\
+			{'message':message, 'time_ms':time_ms})
 
 	def server_error(self, message='', time_ms=0):
 		""" alias for internal_server_error() """
@@ -104,6 +146,7 @@ class ResponseAPI(object):
 	def server_err(self, message='', time_ms=0):
 		""" alias for internal_server_error() """
 		return self.internal_server_error(message, time_ms)
+
 
 
 if __name__ == '__main__':
@@ -115,4 +158,6 @@ if __name__ == '__main__':
 	pprint.pprint(RESPONSE.redirect(time_ms=30))
 	pprint.pprint(RESPONSE.bad_request(time_ms=40))
 	pprint.pprint(RESPONSE.not_found(time_ms=50))
+
 	pprint.pprint(RESPONSE.server_err(time_ms=60))
+	pprint.pprint(RESPONSE.server_error(time_ms=60))
